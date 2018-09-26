37c960dfb8a7dafb8b9bb8d6f5afe958bc9c5d63
Dmytro Stepaniuk <stepaniuk@mlsdev.com>
2018-09-26 15:54:23 +0300

    use gem or app decorator

--- app/decorators/api_authentication/user_decorator.rb
+++ app/decorators/api_authentication/user_decorator.rb
@@ -1,4 +1,4 @@
-class ApiAuthentication::UserDecorator < Draper::Decorator
+class ApiAuthentication::UserDecorator < ( defined?(UserDecorator) ? UserDecorator : Draper::Decorator )
   delegate_all
 
   def as_json *args
