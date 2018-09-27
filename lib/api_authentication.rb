1ea56684a6f048849d5d3eb660a414f4e34135de
Dmytro Stepaniuk <stepaniuk@mlsdev.com>
2018-09-27 19:03:12 +0300

    update authentication

--- lib/api_authentication.rb
+++ lib/api_authentication.rb
@@ -8,8 +8,8 @@ module ApiAuthentication
   autoload :EmailValidator, 'email_validator'
 
   SWAGGER_CLASSES = [
-    '::ApiAuthentication::SessionDocs',
-    '::ApiAuthentication::UnprocessableEntity'
+    ::ApiAuthentication::SessionDocs,
+    ::ApiAuthentication::UnprocessableEntity
   ]
 
   def self.configure(&block)
