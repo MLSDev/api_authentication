97a1114b4288b0dd3351791b087fd64330ce11cc
Dmytro Stepaniuk <stepaniuk@mlsdev.com>
2018-09-27 22:06:52 +0300

    require 'apidocs'

--- lib/api_authentication.rb
+++ lib/api_authentication.rb
@@ -2,6 +2,7 @@ require 'api_authentication/configuration'
 require 'api_authentication/engine'
 require_relative './apidocs'
 require 'apidocs'
+require 'apidocs/session_docs'
 # require 'validators'
 require 'email_validator'
 
