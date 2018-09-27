1fe47d8ce09ff614066e8ef8a53de031f5d6b04f
Dmytro Stepaniuk <stepaniuk@mlsdev.com>
2018-09-27 18:23:55 +0300

    use modules

--- lib/apidocs/session_docs.rb
+++ lib/apidocs/session_docs.rb
@@ -1,56 +1,58 @@
-class ApiAuthentication::SessionDocs
-  require 'apidocs/unprocessable_entity'
-  include Swagger::Blocks
+module ApiAuthentication
+  class SessionDocs
+    require 'apidocs/unprocessable_entity'
+    include Swagger::Blocks
 
-  swagger_path '/session' do
-    operation :post do
-      key :description, 'session'
-      key :summary, 'create session'
-      key :tags, ['session']
-      key :consumes, ['multipart/form-data']
-      parameter do
-        key :name, 'session[email]'
-        key :in, :formData
-        key :required, true
-        key :type, :string
-      end
-      parameter do
-        key :name, 'session[password]'
-        key :in, :formData
-        key :required, true
-        key :type, :string
-        key :format, :password
-      end
-      response '201' do
-        key :description, 'Session'
-      end
-      response '422' do
-        key :description, 'UnprocessableEntity'
-        schema do
-          key :'$ref', :UnprocessableEntity
+    swagger_path '/session' do
+      operation :post do
+        key :description, 'session'
+        key :summary, 'create session'
+        key :tags, ['session']
+        key :consumes, ['multipart/form-data']
+        parameter do
+          key :name, 'session[email]'
+          key :in, :formData
+          key :required, true
+          key :type, :string
+        end
+        parameter do
+          key :name, 'session[password]'
+          key :in, :formData
+          key :required, true
+          key :type, :string
+          key :format, :password
+        end
+        response '201' do
+          key :description, 'Session'
+        end
+        response '422' do
+          key :description, 'UnprocessableEntity'
+          schema do
+            key :'$ref', :UnprocessableEntity
+          end
         end
       end
     end
-  end
 
-  swagger_path '/session' do
-    operation :delete do
-      key :description, 'session'
-      key :summary, 'delete session'
-      key :tags, ['session']
-      key :consumes, ['multipart/form-data']
-      parameter do
-        key :name, 'Authorization'
-        key :in, :header
-        key :default, 'Token token="ACCESS_TOKEN"'
-        key :required, true
-        key :type, :string
-      end
-      response '201' do
-        key :description, 'Session'
-      end
-      response '401' do
-        key :description, 'NotAuthorize'
+    swagger_path '/session' do
+      operation :delete do
+        key :description, 'session'
+        key :summary, 'delete session'
+        key :tags, ['session']
+        key :consumes, ['multipart/form-data']
+        parameter do
+          key :name, 'Authorization'
+          key :in, :header
+          key :default, 'Token token="ACCESS_TOKEN"'
+          key :required, true
+          key :type, :string
+        end
+        response '201' do
+          key :description, 'Session'
+        end
+        response '401' do
+          key :description, 'NotAuthorize'
+        end
       end
     end
   end
--- lib/apidocs/unprocessable_entity.rb
+++ lib/apidocs/unprocessable_entity.rb
@@ -1,25 +1,27 @@
 # frozen_string_literal: true
 
-class ApiAuthentication::UnprocessableEntity
-  include Swagger::Blocks
+module ApiAuthentication
+  class UnprocessableEntity
+    include Swagger::Blocks
 
-  swagger_schema :UnprocessableEntity do
-    property :errors do
-      key :'$ref', :OutputErrors
-    end
-  end
-
-  swagger_schema :OutputErrors do
-    property :base do
-      key :type, :array
-      items do
-        key :type, :string
+    swagger_schema :UnprocessableEntity do
+      property :errors do
+        key :'$ref', :OutputErrors
       end
     end
-    property :error_field do
-      key :type, :array
-      items do
-        key :type, :string
+
+    swagger_schema :OutputErrors do
+      property :base do
+        key :type, :array
+        items do
+          key :type, :string
+        end
+      end
+      property :error_field do
+        key :type, :array
+        items do
+          key :type, :string
+        end
       end
     end
   end
