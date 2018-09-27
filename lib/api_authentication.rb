13654a7a6b0e4f2d66673bf9da889a531bc02d63
Dmytro Stepaniuk <stepaniuk@mlsdev.com>
2018-09-27 22:12:32 +0300

    cleanup

--- lib/api_authentication.rb
+++ lib/api_authentication.rb
@@ -2,14 +2,13 @@ require 'api_authentication/configuration'
 require 'api_authentication/engine'
 require_relative './apidocs'
 require 'apidocs'
-require 'apidocs/session_docs'
 # require 'validators'
 require 'email_validator'
 
 module ApiAuthentication
   autoload :EmailValidator, 'email_validator'
 
-  API_AUTHENTICATION_SWAGGER_CLASSES = [
+  SWAGGER_CLASSES = [
     ::ApiAuthentication::SessionDocs
   ]
 
