52ee29a862f72246aa3c202af987b934791c2b00
Ihor Kobka <kobka@mlsdev.com>
2019-04-22 17:27:52 +0300

    Refresh token creator fix

--- lib/api_authentication/refresh_token_creator.rb
+++ lib/api_authentication/refresh_token_creator.rb
@@ -8,9 +8,10 @@ module ApiAuthentication
     end
 
     def create
-      return unless ApiAuthentication.configuration.include_refresh_token
+      return unless ApiAuthentication.configuration.app_refresh_token_model_class_name
 
-      user.refresh_tokens.create!(
+      ApiAuthentication.refresh_token_model.create!(
+        user_id: user.id,
         token: SecureRandom.base58(100),
         expired_at: ApiAuthentication.configuration.refresh_token_exp,
         ip_address: request.remote_ip,
